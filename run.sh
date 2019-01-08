884bbb82b4d0d7ceeb7dbd5acda7b71c7a988e6e
pau.lopez.78 <pau.lopez.78@gmail.com>
2019-01-08 09:47:20 +0100

    run volume

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 rm -rf ./build-linux
 dotnet publish VotingApp.Api -r linux-x64 -o ../build-linux
+docker run -it -v /$(PWD)/build-linux:/app ubuntu
+
+#apt-get update && apt-get install libicu60 libssl1.0.0 -y

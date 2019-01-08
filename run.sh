90508d5a05442e31c0979beac5fc03735e5714f5
pau.lopez.78 <pau.lopez.78@gmail.com>
2019-01-08 10:06:34 +0100

    port mapping

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 rm -rf ./build-linux
 dotnet publish VotingApp.Api -r linux-x64 -o ../build-linux
-docker run -it -v /$(PWD)/build-linux:/app ubuntu
+docker run -it -p 8080:5000 -v /$(PWD)/build-linux:/app ubuntu-dotnet-deps
 
 #apt-get update && apt-get install libicu60 libssl1.0.0 -y
+#docker commit containerId imageName

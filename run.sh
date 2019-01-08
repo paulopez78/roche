e7a7aeed1eb7f4f3fad73324cd77c9d443fc212e
pau.lopez.78 <pau.lopez.78@gmail.com>
2019-01-08 14:38:23 +0100

    layers cache

--- run.sh
+++ run.sh
@@ -1,21 +1,7 @@
-rm -rf ./build
-docker network create myvotingnetwork
-docker build -f ./VotingApp.Api/Dockerfile -t paulopez/votingapp .
-docker rm -f myvotingapp
-docker run \
-    -d \
-    --network myvotingnetwork \
-    --name myvotingapp \
-    -p 8080:80 \
-    paulopez/votingapp
+docker-compose down
+docker-compose up --build -d
 
 sleep 5
 set -e
-
-docker build -t votingapp-test ./tests
-docker run --rm --network myvotingnetwork -e API_URL=http://myvotingapp/api/voting votingapp-test
-
-docker push paulopez/votingapp
-
-#apt-get update && apt-get install libicu60 libssl1.0.0 -y
-#docker commit containerId imageName
+docker-compose run --rm votingapp-test
+docker-compose push votingapp

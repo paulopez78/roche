d4fbbecd09aeb994016fa156be10fc1f94f6c1ad
pau.lopez.78 <pau.lopez.78@gmail.com>
2019-01-08 10:19:59 +0100

    run docker apnetcore urls

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 rm -rf ./build-linux
 dotnet publish VotingApp.Api -r linux-x64 -o ../build-linux
-docker run -it -p 8080:5000 -v /$(PWD)/build-linux:/app ubuntu-dotnet-deps
+docker run \
+    -w //app \
+    -p 8080:80 \
+    -v /$(PWD)/build-linux:/app \
+    -e ASPNETCORE_URLS=http://+:80 \
+    ubuntu-dotnet-deps bash -c "./VotingApp.Api"
 
 #apt-get update && apt-get install libicu60 libssl1.0.0 -y
 #docker commit containerId imageName

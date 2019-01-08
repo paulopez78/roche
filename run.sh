9b893d412c661df235fe73e7e04435bbef470f10
pau.lopez.78 <pau.lopez.78@gmail.com>
2019-01-08 13:39:51 +0100

    networking

--- run.sh
+++ run.sh
@@ -1,15 +1,19 @@
 rm -rf ./build
-docker build -f Dockerfile -t paulopez/votingapp .
+docker network create myvotingnetwork
+docker build -f ./VotingApp.Api/Dockerfile -t paulopez/votingapp .
 docker rm -f myvotingapp
 docker run \
     -d \
+    --network myvotingnetwork \
     --name myvotingapp \
     -p 8080:80 \
     paulopez/votingapp
 
 sleep 5
 set -e
-./test.sh
+
+docker build -t votingapp-test ./tests
+docker run --rm --network myvotingnetwork -e API_URL=http://myvotingapp/api/voting votingapp-test
 
 docker push paulopez/votingapp
 
